d29be04c725dd9d6cc8930b26ba6408403202f60
Naveena1206 <naveenabaddam1206@gmail.com>
2024-06-16 22:52:22 +0530

    d1nd2

---
+++ README.md
@@ -0,0 +1,47 @@
+# Book record Managment system
+
+## Routes and Endpoints
+
+### /users
+
+POST:Creating a new user
+GET:Get the list of all users
+
+###/users/{id}
+
+GET: Get a  user by ID
+put:update a user by id
+Delete:delete a user by their ID(if he/she have an issued book )(also need to make sure heshe has no penalty)
+
+### /users/subscription-details/{id}
+GET:Get user subscription details
+    >>Data of subscription
+    >>till when its vaild
+    >>Fine if any
+
+## /books
+
+get:get all books
+post:creating / adding a new book
+
+### /books/{id}
+
+get:getting a book by id
+post:update a book by id
+
+### /books/issued
+
+get:Get all issued books
+
+### /books/issued/withFine
+
+get:get all issued books with fine
+
+## subscriptions
+>>basic(3 months)
+>>standard(6 months)
+>>premium(12 months)
+
+npm init
+npm i express
+npm i nodemon --save-dev
---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+
+const {users} = require("./data/users.json");
+
+const app = express();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Server is up and running succesfully"
+    })
+})
+
+/**
+ * Route:/users/:id
+ * method:GET
+ * Description: Create/add new user
+ * Access: public
+ * Parameters: ID
+ */
+
+app.get("/users",(req,res)=>{
+    res.status(200).json({
+        success: true,
+        data: users,
+    })
+})
+
+/**
+ * Route:/users/:id
+ * method:GET
+ * Description: get user by their id 
+ * Access: public
+ * Parameters: ID
+ */
+
+app.get('/users/:id',(req,res)=>{
+    const {id} = req.params;
+    const user = users.find((each)=>each.id===id);
+    if(!user){
+        return  res.status(404).json({
+            success: false,
+            message: "User not Found"
+        })
+    }
+    return res.status(200).json({
+        success: true,
+        data: user
+    })
+
+
+})
+
+/**
+ * Route:/users/:id
+ * method:POST
+ * Description: create/Add a new user
+ * Access: public
+ * Parameters: ID
+ */
+
+app.post("/users",(req,res)=>{
+    const {id , name , surname , email , subscriptionType ,subscriptionDate} =req.body;
+
+    const user = users.find((each)=> each.id== id);
+
+    if(user){
+        return res.status(404).json({
+            success: false,
+            message: "User exit with given ID"
+        })
+    }
+
+    users.push({id , name , surname , email , subscriptionType , subscriptionDate})
+    return res.status(201).json({
+        success: true,
+        data: users,
+    })
+})
+
+/**
+ * Route:/users/:id
+ * method:Put
+ * Description: updating a user by their id 
+ * Access: public
+ * Parameters: ID
+ */
+
+app.put("/users/:id",(req,res)=>{
+    const {id} = req.params;
+    const {data} =req.body;
+
+    const user = users.find((each)=> each.id== id);
+    if(!user){
+        return  res.status(404).json({
+            success: false,
+            message: "User not Found"
+        })
+    }
+
+    const updatedUser = users.map((each)=>{
+        if(each.id===id){
+            return {
+                ...each,
+                ...data
+            }
+        }
+        return each;
+    })
+
+    return res.status(200).json({
+        success: true,
+        data:updatedUser
+    })
+})
+
+
+app.all("*",(req,res)=>{
+    res.status(404).json({
+        message:"This Route doesn't exits "
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running  at a port ${PORT}`)
+})
+
